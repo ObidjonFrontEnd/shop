92797ec632b9307a677789d1538eeb4a3265c7ce
ObidjonFrontEndn <lightline002@gmail.com>
2025-05-12 22:25:36 +0500

    2.2.14

--- app/fillter/components/cotegory/index.tsx
+++ app/fillter/components/cotegory/index.tsx
@@ -1,22 +1,28 @@
 import { CotegorTyp } from '@/app/types/cotegoryType'
-import BrendsCotegory from './brendsCotegory'
 import axios from 'axios'
+import BrendsCotegory from './brendsCotegory'
 
 interface Props {
 	brend?: string
 }
 
-
-
 type CategoryNavProps = {
-	data: CotegorTyp[];
-};
+	data: CotegorTyp[]
+}
 
 const Cotegory = async ({ brend }: Props) => {
+	const respons = await axios.get(
+		'https://api.ashyo.fullstackdev.uz/brands/all'
+	)
+	const apiData: CategoryNavProps[] = await respons.data
 
-		const respons = await axios.get('https://api.ashyo.fullstackdev.uz/brands/all')
-		const cotegory:CategoryNavProps[] = await respons.data
-	
+	// Преобразуем данные в формат, ожидаемый BrendsCotegory
+	const cotegory = apiData.flatMap(item =>
+		item.data.map(brand => ({
+			id: String(brand.id), // Преобразуем number в string
+			name: brand.name,
+		}))
+	)
 
 	return (
 		<div className='w-[280px] h-full  p-[19px] min-h-[600px] rounded-[8px] bg-[#EBEFF3]'>
