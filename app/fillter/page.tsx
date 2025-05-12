ad62ade3bbdc4c19e458c59aebb36bba0e30dc63
ObidjonFrontEndn <lightline002@gmail.com>
2025-05-12 21:30:25 +0500

    2.2.2

--- app/fillter/page.tsx
+++ app/fillter/page.tsx
@@ -1,21 +1,22 @@
-import CardsFiler from './components/cards'
-import Cotegory from './components/cotegory'
+import CardsFiler from './components/cards';
+import Cotegory from './components/cotegory';
 
-export default function FillterPage({
-  searchParams,
-}: {
-  searchParams: { brend?: string };  
-}) {
-  const brend = searchParams.brend;
+interface FillterPageProps {
+  searchParams?: {
+    brend?: string;
+  };
+}
+
+export default function FillterPage({ searchParams }: FillterPageProps) {
+  const brend = searchParams?.brend;
 
   return (
-    <div className='p-4 max-w-[1180px] px-[10px] mx-auto mt-[10px]'>
-      <div className='flex gap-[21px] h-full'>
-        <div className='hidden md:block h-full'>
+    <div className="p-4 max-w-[1180px] px-[10px] mx-auto mt-[10px]">
+      <div className="flex gap-[21px] h-full">
+        <div className="hidden md:block h-full">
           <Cotegory brend={brend} />
         </div>
-
-        <div className='w-full h-full'>
+        <div className="w-full h-full">
           <CardsFiler brend={brend} />
         </div>
       </div>
